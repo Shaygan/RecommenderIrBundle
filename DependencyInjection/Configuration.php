61606983e2f562ca1e5e627b1d3f8248aecc4ef0
Iman Ghasrfakhri <iman@i-gh.ir>
2016-07-18 12:33:15 +0430

    Fix File name

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,7 +17,7 @@ class Configuration implements ConfigurationInterface {
      */
     public function getConfigTreeBuilder() {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('shaygan_recommender');
+        $rootNode = $treeBuilder->root('shaygan_recommender_ir');
 
 
         $rootNode
